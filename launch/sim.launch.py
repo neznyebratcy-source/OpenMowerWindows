b0638c3c3a751895d2a2ae25b9124fc6b3690c2a
neznyebratcy-source <neznyebratcy@gmail.com>
2026-03-30 18:22:42 +0300

    Отключил графику Gazebo чтобы спасти память

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -128,7 +128,7 @@ def generate_launch_description():
                 [os.path.join(get_package_share_directory('ros_gz_sim'),
                               'launch', 'gz_sim.launch.py')]),
             launch_arguments={
-                'gz_args': '-r -v 6 {}'.format(world_path),
+                'gz_args': '-r -s -v 6 {}'.format(world_path),
             }.items()),
         RegisterEventHandler(
             event_handler=OnProcessExit(
